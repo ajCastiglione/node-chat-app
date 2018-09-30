d6cac7feefc658dc2fef4a426965fafcb6ad964e
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-09-30 11:55:09 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on("connect", function() {
   console.log("Connected to server");
-
-  socket.emit("createMessage", {
-    from: "Dude",
-    text: "hey, sending over message from client"
-  });
 });
 
 socket.on("disconnect", function() {
